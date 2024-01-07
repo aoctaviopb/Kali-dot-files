c32c2058fc240238e2efc0f983e77022d6a3bed4
aoctaviopb <aoctaviopb@gmail.com>
2024-01-07 00:34:16 -0500

    Initial dot files for Kali-Linux with a fresh instalation in a VM

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "********************************************************"
+echo "Installing i3-wm and others..."
+echo "********************************************************"
+sudo apt install i3 rofi alacritty polybar
+
+
+echo "********************************************************"
+echo "Deleting original conf files"
+echo "********************************************************"
+rm ~/.config/i3/config
+rm ~/.config/polybar/forest/config.ini
+rm ~/.config/polybar/forest/modules.ini
+
+echo "********************************************************"
+echo "Creating symbolic links"
+echo "********************************************************"
+ln -s ~/dot_Files/i3_config ~/.config/i3/config
+ln -s ~/dot_Files/polybar_fores_conf ~/.config/polybar/forest/config.ini
+ln -s ~/dot_Files/polybar_fores_modules ~/.config/polybar/forest/modules.ini
+
+#edited files:
+#./config/polybar/forest/modules.ini
+#module network
+#lines 379, 763, 821
+
+#./config/polybar/forest/config.ini
+#lines 143, 145
+
+#./config/i3/config
